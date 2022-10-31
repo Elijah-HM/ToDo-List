71c5efd8efc740b87b05680a34ea5de67d191c43
Elijah Muhammad <elijahhm98@gmail.com>
2022-10-31 13:16:58 -0400

    DOMContentLoaded

--- app.js
+++ app.js
@@ -1,19 +1,36 @@
-const form = document.querySelector('form.add');
-const addButton = document.querySelector('#addButton');
-const itemToAdd = document.querySelector('#itemToAdd');
-const ul = document.querySelector('.todo');
+// Steps (write down steps of what you want to do) 
+// 1. user adds task in textbox then click on button
+// 2. We capture the text in th input(textbox)
+// 3. display that text on the screen 
+  // create the new li element
+  // add the usertext to the element
+  // add the element as a child to the ul
+// 4. when delete is clicked, delete the item 
+  // add a delete button
+  // delete button on click should remove the item
+    // add event listener for delete button
+    // delete the parent
+// 5. after item is added clear the textfield
+// 6. auto focus after we add a item. 
+// 7. enable enter-to-submit. (when ever we hit enter the whole page refresh. we dont want that)
 
-form.addEventListener('submit', function(event){
-  event.preventDefault();
-   if(itemToAdd.value){
-    const newItem = createItem(itemToAdd.value)
-    ul.appendChild(newItem);
-    itemToAdd.value = '';
-    itemToAdd.focus();
-  }
-})
+document.addEventListener('DOMContentLoaded', function(){
+  const form = document.querySelector('form.add');
+  const addButton = document.querySelector('#addButton');
+  const itemToAdd = document.querySelector('#itemToAdd');
+  const ul = document.querySelector('.todo');
 
-function createItem(val){
+  form.addEventListener('submit', function(event){
+    event.preventDefault();
+    if(itemToAdd.value){
+      const newItem = createItem(itemToAdd.value)
+      ul.appendChild(newItem);
+      itemToAdd.value = '';
+      itemToAdd.focus();
+    }
+  })
+
+  function createItem(val){
     const item = document.createElement('li');
     const span = document.createElement('span');
     const delBtn = document.createElement('button')
@@ -30,4 +47,6 @@ function createItem(val){
     })
 
     return item;
-}
+  }
+})
+
